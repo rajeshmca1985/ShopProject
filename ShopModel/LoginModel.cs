d475cbda47eca55343e1bc5b9d4f1cc4024d96fc
sameer <sameer.inapp@gmail.com>
2017-03-10 16:18:28 +0530

    login model added

---
+++ ShopModel/LoginModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ShopModel
+{
+    public class LoginModel
+    {
+        public int Userid { get; set; }
+        //public string FullName { get; set; }
+        [Required(ErrorMessage = "Username required", AllowEmptyStrings = false)]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Password Required", AllowEmptyStrings = false)]
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Password)]
+        public string Password { get; set; }
+    }
+}
