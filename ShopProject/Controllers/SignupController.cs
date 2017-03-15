5d8ad6ce0f6493ae37565fc7e17c25514f0a7d46
Rajesh <kavya.sasikala@inapp.com>
2017-03-15 14:22:26 +0530

    submit event and signup controller

---
+++ ShopProject/Controllers/SignupController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShopProject.Controllers
+{
+    public class SignupController : Controller
+    {
+        //
+        // GET: /Signup/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Signup()
+        {
+            return View();
+        }
+    }
+}
